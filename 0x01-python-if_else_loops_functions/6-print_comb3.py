3c4f75070a9f470c04e15e251a9e4b7992698524
Eden Kitaw <edenkitaw19@gmail.com>
2022-06-01 00:48:10 +0300

    commited

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
         if i == 8:
-            print(f"{i}{j}")
+            print("{}{}".format(i, j))
         else:
-            print(f"{i}{j}", end=", ")
+            print("{}{}".format(i, j), end=", ")
