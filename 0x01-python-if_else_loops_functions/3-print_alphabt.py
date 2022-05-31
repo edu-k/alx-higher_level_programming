3352a340eaab5973de9cc7c51c1f1de07c737262
Eden Kitaw <edenkitaw19@gmail.com>
2022-06-01 00:30:28 +0300

    commited

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
-#!/use/bin/python
+#!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if i != ord('q') and i != ord('e'):
-        print(f"{i:c}", end='')
+    if i != ord('e') and i != ord('q'):
+        print("{:c}".format(i), end="")
