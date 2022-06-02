021c128df9b96e242a8752f4ec946470d5632d78
Eden Kitaw <edenkitaw19@gmail.com>
2022-06-02 21:51:51 +0300

    commited

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    idx = len(sys.argv) - 1
+    print(f"{idx} {"argument" if idx == 1 else "arguments"}{'.' if idx == 0 else ':'}")
+    for i in range(idx):
+        print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
