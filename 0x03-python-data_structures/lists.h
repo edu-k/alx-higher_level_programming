0d4a3a4c80002de60386738d81bcb5dc1d5ce46a
Eden Kitaw <edenkitaw19@gmail.com>
2022-06-05 22:09:24 +0300

    commited

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -4,14 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+/**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for project
- *
  */
 typedef struct listint_s
 {
